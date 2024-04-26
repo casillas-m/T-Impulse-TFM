c1fd2c3e55462b55813c32fa82bad160d9f5704d
casillas-m <56522545+casillas-m@users.noreply.github.com>
2024-04-26 19:00:46 +0200

    Changes to support two devices

--- src/oled.cpp
+++ src/oled.cpp
@@ -14,7 +14,9 @@ void oled_init(void)
     u8g2->setFontMode(1); // Transparent
     u8g2->setFontDirection(0);
     u8g2->setFont(u8g2_font_fub14_tf);
-    u8g2->drawStr(2, 18, "LilyGo");
+    char dev_name[5];
+    sprintf(dev_name,"TFM %d",DEV_NAME);
+    u8g2->drawStr(1, 18, dev_name);
     u8g2->sendBuffer();
     for (int i = 0; i < 0xFF; i++)
     {
@@ -29,8 +31,8 @@ void oled_init(void)
 
 void oled_sleep(void) {
   u8g2->clearBuffer();
-  u8g2->setFont(u8g2_font_IPAandRUSLCD_tr);
-  u8g2->drawStr(12, 8, "Sleep");
+  u8g2->setFont(u8g2_font_fub14_tf);
+  u8g2->drawStr(2, 24, "Sleep");
   u8g2->sendBuffer();
   delay(3000);
   u8g2->sleepOn();
